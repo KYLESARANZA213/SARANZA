f01ac997213bd91c4661d4c6968d8a2d839d3452
KYLE213 <kylesaranza526@gmail.com>
2025-11-10 01:57:00 +0000

    commit

--- maze.py
+++ maze.py
@@ -1,46 +1,30 @@
-print("Welcome to SARANZA's Maze")
+player_x = 0
+player_y = 0
 
-maze = [
-    "#########",
-    "#P     G#",
-    "# ### ###",
-    "#   #   #",
-    "#########"
-]
+treasure_x = 7
+treasure_y = 3
+game_running = True
 
-player_row, player_col = 1, 1  # Starting position
 
-def print_maze():
-    for row in maze:
-        print(row)
-    print("Use W/A/S/D to move. Q to quit.\n")
+print(f"Find the treasure at ({treasure_x}, {treasure_y})!")
 
-while True:
-    print_maze()
-    move = input("Move: ").lower()
-    if move == 'q':
-        print("Goodbye!")
-        break
 
-    new_row, new_col = player_row, player_col
-    if move == 'w': new_row -= 1
-    elif move == 's': new_row += 1
-    elif move == 'a': new_col -= 1
-    elif move == 'd': new_col += 1
+while game_running:
+    move = input("Enter move (up/down/right/left for movement, q to quit): ").lower()
+
+    if move == 'up':
+        player_y += 1
+    elif move == 'down':
+        player_y -= 1
+    elif move == 'left':
+        player_x -= 1
+    elif move == 'right': 
+        player_x += 1
+    elif move == 'q':
+        game_running = False
+        print("You quit the game.")
     else:
-        print("Invalid input.")
-        continue
+        print("Invalid move. Please use up/down/right/left for movement, or q to quit.")
 
-    if maze[new_row][new_col] == '#':
-        print("You hit a wall!\n")
-        continue
-    elif maze[new_row][new_col] == 'G':
-        print("🎉 You reached the goal! You win!")
-        break
-    else:
-        maze[player_row] = maze[player_row][:player_col] + ' ' + maze[player_row][player_col+1:]
-        maze[new_row] = maze[new_row][:new_col] + 'P' + maze[new_row][new_col+1:]
-        player_row, player_col = new_row, new_col
-
-
-
+    
+    print(f"Player position: ({player_x}, {player_y})")
